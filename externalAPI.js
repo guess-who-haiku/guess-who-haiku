8cfb360b38d03c0faa3582da511c4cdc773cf682
srajiang <srajiang@gmail.com>
2020-01-23 23:55:35 -0800

    Add working getAuthorSelection fetching from mongo

--- externalAPI.js
+++ externalAPI.js
@@ -9,11 +9,12 @@ const library = {};
 async function buildBarsLibrary() {
   let i = 0;
   
-  while (i < 5) {
+  while (i < 20) {
     
     const payload = await fetchBars();
     // console.log(payload);
     
+    if ( payload === undefined ) continue;
     let { lyric, author } = payload.data.data
     
     let parsed = lyric.split('\n').join('. ');
@@ -23,7 +24,6 @@ async function buildBarsLibrary() {
     } else {
       library[author] += ` ${parsed}`;
     }
-  
     i++;
   }
 
@@ -33,12 +33,21 @@ async function buildBarsLibrary() {
 
 async function fetchBars() {
   
-  const payload = await axios.get(
-    "https://a3odwonexi.execute-api.us-east-2.amazonaws.com/default/Bars_API?method=getQuote&category=sfw"
-    
-    )
-    return payload;
-    
+    try {  
+      const payload = await axios.get(
+        "https://a3odwonexi.execute-api.us-east-2.amazonaws.com/default/Bars_API?method=getQuote&category=sfw",
+        {
+          params: {
+            "method": "getQuote",
+            "category": ["sfw", "ludacris"]
+          }
+        }
+      )
+       
+        return payload; 
+    } catch {
+      console.log("Error fetching from Bars API")
+    }
   }
   
   async function fetchLoremRicksum() {
@@ -54,7 +63,7 @@ async function fetchBars() {
       }
       );
       
-      library["rick and morty"] = payload.data.data[0];
+      library["Rick and Morty"] = payload.data.data[0];
       // console.log(library);
   }
     
@@ -64,8 +73,8 @@ async function fetchBars() {
       'https://api.whatdoestrumpthink.com/api/v1/quotes'
       );
       
-      library["donald trump"] = payload.data.messages.personalized.join('. ')
-      library["donald trump"] = payload.data.messages.non_personalized.join('. ')
+      library["Donald Trump"] = payload.data.messages.personalized.join('. ')
+      library["Donald Trump"] = payload.data.messages.non_personalized.join('. ')
       
       // console.log(library);
         
@@ -77,19 +86,87 @@ async function fetchBars() {
       "https://quote-garden.herokuapp.com/quotes/all"
       );
       
-      // console.log(payload.data);
+     
       let quotesArray = payload.data.results;
       
       for (let i = 0; i < quotesArray.length; i++) {
 
         while (quotesArray[i].quoteAuthor.includes(".")) {
-          // console.log(quotesArray[i].quoteAuthor)
+
           quotesArray[i].quoteAuthor = quotesArray[i].quoteAuthor.replace(".", "");
         }
         library[quotesArray[i].quoteAuthor] = quotesArray[i].quoteText;
       }  
   }
         
+  async function fetchKanye() {
+
+    for (let i = 0; i < 100; i++) {
+      
+      const payload = await axios.get(
+        "https://api.kanye.rest"
+      );
+      
+      if (library["Kanye West"] === undefined ) {
+        library["Kanye West"] = payload.data.quote;
+      } else {
+
+        library["Kanye West"] += `. ${payload.data.quote}`;
+      }
+
+    }
+  }
+
+  async function fetchSimpsons() {
+
+    for (let i = 0; i < 10; i++) {
+
+      const payload = await axios.get(
+        "https://thesimpsonsquoteapi.glitch.me/quotes", 
+        {
+          params: {
+            count: 10
+          }
+        }
+      );
+
+      payload.data.forEach((quoteBody) => {
+
+        let parsedCharacter;
+
+        if (quoteBody.character.includes(".")) {
+          parsedCharacter = quoteBody.character.replace(".", "");
+        } else {
+          parsedCharacter = quoteBody.character;
+        }
+
+          if (library[parsedCharacter] === undefined) {
+            library[parsedCharacter] = quoteBody.quote;
+          } else {
+            library[parsedCharacter] += ` ${quoteBody.quote}`;
+          } 
+      })
+    }
+  }
+
+  async function fetchGOT() {
+
+      for (let i = 0; i < 100; i++) {
+        const payload = await axios.get(
+          "https://got-quotes.herokuapp.com/quotes"
+        );
+
+    
+        if (library["Game of Thrones"] === undefined) {
+          library["Game of Thrones"] = payload.data.quote;
+        } else {
+          library["Game of Thrones"] += ` ${payload.data.quote}`;
+        }
+      }
+      console.log(library);
+
+  }
+
                   
   async function constructLibrary() {
     
@@ -98,26 +175,28 @@ async function fetchBars() {
       await fetchQuoteGarden();
       await fetchTrumpism();
       await fetchLoremRicksum();
-      await buildBarsLibrary();
+      await fetchKanye();
+      await fetchSimpsons();
+      await fetchGOT();
     
-      console.log('HERE IS THE LIBRARY', library);
+      // console.log('HERE IS THE LIBRARY', library);
 
       newLibrary = new Library({
         library
       })
-      debugger;
+
       newLibrary
         .save()
-        .then(newLibrary => console.log("BIG DATABASE ENERGY", newLibrary))
+        .then(newLibrary => console.log("Successfully saved library to db", newLibrary))
         .catch(err => console.log(err));
 
     } catch {
-      console.log('BIG ERROR ENERGY ')
+      console.log('Error')
     }
   }
 
   module.exports = constructLibrary;
                   
-                  
+  
                   
                   
