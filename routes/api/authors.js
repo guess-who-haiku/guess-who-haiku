1d7280bc598dfd59561322aeb550039d911d7a7d
srajiang <srajiang@gmail.com>
2020-01-24 09:06:20 -0800

    Add tweak to pull authors from Library instead of Author table

--- routes/api/authors.js
+++ routes/api/authors.js
@@ -3,15 +3,26 @@ const router = express.Router();
 const passport = require("passport");
 
 const Author = require("../../models/Author");
+const Library = require("../../models/Library")
 
 // ------------------------------- GET /
+// router.get(
+//   "/",
+//   (req, res) => {
+//     Author.find()
+//       .then(authors => res.json(authors))
+//       .catch(err => res.status(404).json({ noauthorsfound: "No authors found" }));
+//   }
+// );
+
 router.get(
   "/",
   (req, res) => {
-    Author.find()
+    Library.find()
       .then(authors => res.json(authors))
-      .catch(err => res.status(404).json({ noauthorsfound: "No authors found" }));
+      .catch(err => res.status(404).json({ noauthorsfound: "No authors found in library" }));
   }
 );
+
 
 module.exports = router;
