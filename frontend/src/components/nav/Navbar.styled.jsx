eabb1f4b44b2a921a6bf07a3cce188c72222d2f9
Alex Segers <a.segers.dev@gmail.com>
2020-01-26 14:58:52 -0800

    update manifest file, remove undesired data properties in state, add global rem responsive styles and modify Nav styles

--- frontend/src/components/nav/Navbar.styled.jsx
+++ frontend/src/components/nav/Navbar.styled.jsx
@@ -1,12 +1,12 @@
 import styled from 'styled-components';
 import styledMap from 'styled-map';
-import theme, { media } from 'styled/theme';
+import { media, colors, font } from 'styled/theme';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faBars as menuIcon } from '@fortawesome/free-solid-svg-icons';
 import { Link, NavLink } from 'react-router-dom'
 
 export const Nav = styled.nav`
-  background-color: ${theme.colors.primary};
+  background-color: ${colors.primary};
   padding: .9rem 1.2rem;
   display: grid;
   /* grid-gap: 20px; */
@@ -14,17 +14,30 @@ export const Nav = styled.nav`
   align-items: center;
 
   & > * {
-    color: ${theme.colors.utilityYellow};
+    color: ${colors.utilityYellow};
   }
 `;
 
-export const Logo = styled(Link).attrs({ to: '/' })`
-  font-weight: ${theme.font.weights.extraBold};
-  font-size: 3.5rem;
+export const LogoBox = styled(Link).attrs({ to: '/' })`
+  ${media.desktopS`
+    justify-self: start;
+    padding-left: 1rem;
+  `}
+`;
+
+export const Logo = styled.img.attrs({ alt: 'Guess Who, Haiku - Logo', src: '/logo512.png' })`
+  width: 5rem;
+  ${media.tablet`
+    width: 10rem;
+  `}
 `;
 
 export const MenuIcon = styled(FontAwesomeIcon).attrs({ icon: menuIcon })`
   font-size: 3rem;
+  ${media.tablet`
+    margin-left: 2rem;
+    font-size: 4.5rem;
+  `}
   ${media.desktopS`
     display: none;
   `}
@@ -32,7 +45,7 @@ export const MenuIcon = styled(FontAwesomeIcon).attrs({ icon: menuIcon })`
 
 // ---------------- Menu
 export const Menu = styled.ul`
-  background-color: ${theme.colors.primary};
+  background-color: ${colors.primary};
   width: 100%;
   display: flex;
   flex-direction: column;
@@ -60,10 +73,20 @@ export const Menu = styled.ul`
 `;
 
 export const MenuLink = styled.a`
+  font-size: 2.5rem;
   padding: .9rem 1.2rem;
+  &::hover {
+    ${font.weights.extraBold};
+  }
 `;
 
-export const MenuNavLink = styled(MenuLink).attrs({ as: NavLink })``;
+export const MenuNavLink = styled(MenuLink).attrs({ as: NavLink, activeClassName: 'selected' })`
+  
+  &.selected {
+    color: ${colors.utilityYellow};
+    font-weight: ${font.weights.bold};
+  }
+`;
 
 export const MenuItem = styled.li`
   list-style: none;
--- frontend/src/store/haikus/actions.js
+++ frontend/src/store/haikus/actions.js
@@ -19,36 +19,36 @@ export const Thunks = {};
 
 Thunks.fetchHaiku = (haikuId) => dispatch =>
   APIUtil.getHaiku(haikuId)
-    .then(haiku => dispatch(Creators.receiveHaiku(haiku)))
+    .then(({ data: haiku }) => dispatch(Creators.receiveHaiku(haiku)))
     .catch(err => console.log(err));
 
 Thunks.fetchHaikusUser = (userId) => dispatch =>
   APIUtil.getHaikusUser(userId)
-    .then(haikus => dispatch(Creators.receiveHaikus(haikus)))
+    .then(({ data: haikus }) => dispatch(Creators.receiveHaikus(haikus)))
     .catch(err => console.log(err));
 
 Thunks.fetchHaikuChallenges = (haikus) => dispatch =>
   APIUtil.getHaikuChallenges(haikus)
-    .then(haikus => dispatch(Creators.receiveHaikus(haikus)))
+    .then(({ data: haikus }) => dispatch(Creators.receiveHaikus(haikus)))
     .catch(err => console.log(err));
 
 Thunks.createHaiku = (haiku) => dispatch =>
   APIUtil.createHaiku(haiku)
-    .then(haiku => dispatch(Creators.receiveHaiku(haiku)))
+    .then(({ data: haiku }) => dispatch(Creators.receiveHaiku(haiku)))
     .then(haiku => dispatch(NewHaikuCreators.receiveNewHaiku(haiku)))
     .catch(err => console.log(err));
 
 Thunks.deleteHaiku = (haikuId) => dispatch =>
   APIUtil.deleteHaiku(haikuId)
-    .then(({ _id }) => dispatch(Creators.removeHaiku(_id)))
+    .then(({ data: { _id } }) => dispatch(Creators.removeHaiku(_id)))
     .catch(err => console.log(err));
 
 Thunks.createHaikuShares = (haikuId, recipientIds) => dispatch =>
   APIUtil.createHaikuShares(haikuId, recipientIds)
-    .then(haiku => dispatch(Creators.receiveHaiku(haiku)))
+    .then(({ data: haiku }) => dispatch(Creators.receiveHaiku(haiku)))
     .catch(err => console.log(err));
 
 Thunks.updateHaikuShare = (haikuId, userId, openTS, completeTS) => dispatch =>
   APIUtil.updateHaikuShare(haikuId, userId, openTS, completeTS)
-    .then(haiku => dispatch(Creators.receiveHaiku(haiku)))
+    .then(({ data: haiku }) => dispatch(Creators.receiveHaiku(haiku)))
     .catch(err => console.log(err));
