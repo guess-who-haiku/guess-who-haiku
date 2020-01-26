1e7de921eb9833c7365c22ff90683cdc112aac71
tfaramarzi <tfaramarzi@gmail.com>
2020-01-26 00:02:04 -0800

    Add styling to haiku builder

--- frontend/src/components/haiku_builder/HaikuBuilder.jsx
+++ frontend/src/components/haiku_builder/HaikuBuilder.jsx
@@ -1,10 +1,10 @@
 import React, { useState, useEffect } from 'react';
 import Loader from 'react-loader-spinner';
 
-import { HBContainer } from './HaikuBuilder.styled';
+import { HBContainer, LIContainer, Message, ErrorMsg } from './HaikuBuilder.styled';
 import { formatHaiku } from '../../util/haiku_format_util';
 
-const HaikuBuilder = ({createHaiku, createHaikuShares, fetchAuthors, fetchNewHaiku, authors, newHaiku, users, openModal}) => {
+const HaikuBuilder = ({createHaiku, createHaikuShares, fetchAuthors, fetchNewHaiku, authors, newHaiku, users, openModal, currentUser}) => {
 
     //fetch authors on load
     useEffect(() => {
@@ -16,9 +16,9 @@ const HaikuBuilder = ({createHaiku, createHaikuShares, fetchAuthors, fetchNewHai
     const [haiku, setHaiku] = useState([]);
     const [step, setStep] = useState(0);
     const [reverse, setReverse] = useState(false);
+    const [authorError, setAuthorError] = useState(false);
 
     console.log(haikuAuthors);
-   
 
     //update selection of haiku authors
     const handleAuthorSelection = e => {
@@ -28,28 +28,42 @@ const HaikuBuilder = ({createHaiku, createHaikuShares, fetchAuthors, fetchNewHai
         } else if (haikuAuthors.includes(newAuthor)) {
             setHaikuAuthors(haikuAuthors.filter(author => (author !== newAuthor)) )
         }
+        if (haikuAuthors.length > 0) {
+            setAuthorError(false)
+        }
     };
 
     //create new haiku
     const generateHaiku = () => {
-        fetchNewHaiku(haikuAuthors)
+        console.log(haikuAuthors.length);
+        if (haikuAuthors.length === 0) {
+            setAuthorError(true)
+        } else {
+            fetchNewHaiku(haikuAuthors)
+            toggleNext();
+        } 
     };
 
     useEffect(() => {
         newHaiku && setHaiku(formatHaiku(newHaiku.data, haikuAuthors))
-        //newHaiku && console.log('inside useEffect', newHaiku)
     }, [newHaiku])
 
     //load new haiku
     useEffect(() => {
         if (step === 1) {
-            console.log('step is 1')
             const loading = setTimeout(() => {
                 toggleNext()
             }, 2000);
             return () => clearTimeout(loading);
         }
     }, [step])
+
+    //save haiku
+    // useEffect(() => {
+    //     if (currentUser) {
+
+    //     }
+    // }, [currentUser])
 
     //start over
     const startOver = () => {
@@ -72,31 +86,38 @@ const HaikuBuilder = ({createHaiku, createHaikuShares, fetchAuthors, fetchNewHai
         }
     };
 
+    //
+    const error = <ErrorMsg>Please select at least one author</ErrorMsg>
+
     //steps
     const ChooseAuthors = () => (
         <>
-            <p>Choose up to three figures below:</p>
-            <ul>
+            <Message>Choose up to three figures below:</Message>
+            <LIContainer>
                 {authors.data && authors.data.map(author => (
                     <li data-selected={haikuAuthors.includes(author)} key={author} data-name={author} onClick={handleAuthorSelection}>
                         {author}
                         {/* <img src={author} alt={author} /> */}
                     </li>
                 ))}
-            </ul>
-            <button onClick={() => { generateHaiku(); toggleNext(); }}>Build my Haiku!</button>
+            </LIContainer>
+            {authorError ? error : null}
+            <button onClick={generateHaiku}>Build my Haiku!</button>
+            
         </>
     );
 
     const GeneratingHaiku = () => (
         <>
-            <h1>Just one moment while we build your haiku...</h1>
-            <Loader
-                type="Grid"
-                color="#9277B2"
-                height={80}
-                width={80}
-            />
+            <Message>Good choice! Just one moment while we build your haiku...</Message>
+            <LIContainer>
+                <Loader
+                    type="Grid"
+                    color="#f9cc10"
+                    height={80}
+                    width={80}
+                />
+            </LIContainer>
         </>
     );
 
@@ -111,21 +132,21 @@ const HaikuBuilder = ({createHaiku, createHaikuShares, fetchAuthors, fetchNewHai
             </div>
             <button onClick={() => { generateHaiku(); toggleBack(); }}>Regenerate haiku</button>
             <button onClick={startOver}>Let me start over</button> 
-            <button onClick={() => openModal('test')}>Save for later</button>
-            <button onClick={() => openModal('test')}>Share now</button>   
+            <button onClick={() => openModal('login')}>Save for later</button>
+            <button onClick={() => openModal('login')}>Share now</button>   
         </>
     );
 
     const ShareHaiku = () => (
         <>
-            <p>Challenge a friend (or a few friends) to solve your haiku by choosing them below, or generating a link and sending it to them.</p>
-            <ul>
+            <Message>Challenge your friends to solve your haiku by choosing them below, or generating a link to share with them!</Message>
+            <LIContainer>
                 {users && users.map(user => (
                     <li data-selected={haikuShares.includes(user.username)} key={user.username} data-username={user.username} onClick={handleShareSelection}>
                         <strong>{user.username}</strong>
                     </li>
                 ))}
-            </ul>
+            </LIContainer>
             <button>Share</button>
             {/* set input value to current haiku id */}
             <input type="text" name="link"/>
@@ -134,7 +155,6 @@ const HaikuBuilder = ({createHaiku, createHaikuShares, fetchAuthors, fetchNewHai
     );
 
     const Steps = [ChooseAuthors, GeneratingHaiku, GeneratedHaiku, ShareHaiku];
-    console.log("HAIKU:", haiku)
 
     const toggleBack = () => {
         let prevStep = step - 1 < 0 ? Steps.length - 1 : step - 1;
@@ -143,7 +163,6 @@ const HaikuBuilder = ({createHaiku, createHaikuShares, fetchAuthors, fetchNewHai
     };
 
     const toggleNext = () => {
-        console.log('toggled next!')
         let nextStep = step + 1 < Steps.length ? step + 1 : 0;
         setStep(nextStep);
         setReverse(false);
@@ -151,7 +170,6 @@ const HaikuBuilder = ({createHaiku, createHaikuShares, fetchAuthors, fetchNewHai
     
     return (
         <HBContainer>
-            {/* <ChooseAuthors /> */}
             {React.createElement(Steps[step])}
         </HBContainer>
     )
--- frontend/src/components/haiku_builder/HaikuBuilder.styled.js
+++ frontend/src/components/haiku_builder/HaikuBuilder.styled.js
@@ -2,5 +2,34 @@ import styled from 'styled-components';
 import theme from 'styled/theme';
 
 export const HBContainer = styled.div`
-    background-color: ${theme.colors.toryBlue};    
+    background-color: ${theme.colors.primary};
+    border: 1rem solid ${theme.colors.primaryDark};
+    font-weight: 200;
+    line-height: 1.5;  
+    margin: 4rem 5rem;
+    min-height: 70%;
 `;
+
+export const LIContainer = styled.div`
+    display: flex;
+    justify-content: center;
+    flex-flow: row wrap;
+    padding: 2rem;
+`;
+
+export const Message = styled.h3`
+    color: ${theme.colors.utilityYellow};
+    font-size: 2.5rem; 
+`;
+
+export const ErrorMsg = styled.span`
+    font-size: 1.6rem;
+    height: 3rem;
+    padding: 1rem 0;
+    color: ${theme.colors.utilityRed};
+    font-weight: ${theme.font.weights.bold};
+`;
+
+// export const Button = styled.button`
+
+// `
