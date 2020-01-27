43ef780fafc69c62c5088a2be35af996dd4d5d99
tfaramarzi <tfaramarzi@gmail.com>
2020-01-26 16:30:12 -0800

    Modify haiku builder for removal of data key

--- frontend/src/store/selectors.js
+++ frontend/src/store/selectors.js
@@ -1,2 +1,3 @@
 export const selectCurrentUser = ({entities, session}) => entities.users[session.userId];
 export const selectAllUsers = ({entities}) => Object.values(entities.users);
+export const selectAllAuthors = ({entities}) => Object.values(entities.authors);
