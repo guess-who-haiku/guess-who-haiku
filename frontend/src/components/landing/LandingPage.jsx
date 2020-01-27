c3bc8ec174e8f8a78a61f0d7b09d69c15fef97ea
tfaramarzi <tfaramarzi@gmail.com>
2020-01-26 21:09:42 -0800

    Update haiku builder to save haiku and begin building share functionality

--- frontend/src/components/landing/LandingPage.jsx
+++ frontend/src/components/landing/LandingPage.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { Page, Title, DescriptionBox } from './LandingPage.styled';
 import HaikuBuilder from '../haiku_builder/HaikuBuilder';
 
-const LandingPage = ({ openModal, createHaikuShares, createHaiku, fetchAuthors, fetchNewHaiku, fetchUsers, currentUser, authors, newHaiku, users }) => {
+const LandingPage = ({ openModal, createHaikuShare, createHaiku, fetchAuthors, fetchNewHaiku, fetchUsers, currentUser, authors, newHaiku, users }) => {
 
   let description = currentUser ? `Welcome back! Conjure up a new haiku below:` : "Welcome to Guess Who, Haiku! Craft outstanding haikus with the help of the witsters of today’s popular culture, and challenge your friends to be the first to GUESS WHO the voice is behind your newfound literary genius.";
 
@@ -15,7 +15,7 @@ const LandingPage = ({ openModal, createHaikuShares, createHaiku, fetchAuthors, 
 
       <HaikuBuilder 
         createHaiku={createHaiku} 
-        createHaikuShares={createHaikuShares} 
+        createHaikuShare={createHaikuShare} 
         fetchAuthors={fetchAuthors} 
         fetchNewHaiku={fetchNewHaiku} 
         authors={authors} 
