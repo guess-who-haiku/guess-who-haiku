b051649b78600f0ae4c449130bb4fa14f0f2c56d
tfaramarzi <tfaramarzi@gmail.com>
2020-02-04 15:42:12 -0800

    Add syllable counting algorithm to Markov chain generator

--- markov.js
+++ markov.js
@@ -72,6 +72,35 @@ function generateLines(dictionaries) {
     return finalObj;
 }
 
+function countSyllables(word) {
+    let syl_count = 0;
+    let vowels = "aeiouyAEIOUY";
+    let secondVow = "aouAOU"
+
+    if (vowels.includes(word[0])) {
+        syl_count += 1
+    }
+    for (let i = 1; i < word.length; i++) {
+        let letter = word.charAt(i);
+        if (vowels.includes(letter) && !vowels.includes(word.charAt(i - 1))) {
+            syl_count += 1;
+        }
+        if ((letter === "i" || letter === "u") && secondVow.includes(word.charAt(i + 1))) {
+            syl_count += 1;
+        }
+    }
+    if (word[word.length - 1] === "e") {
+        syl_count -= 1;
+    }
+    if ((word.substring(word.length - 2) === "le") && (!vowels.includes(word[word.length - 3])) && word.length > 2) {
+        syl_count += 1;
+    }
+    if (syl_count === 0) {
+        syl_count += 1
+    }
+    return syl_count;
+}
+
 // let hptext = "Nearly ten years had passed since the Dursleys had woken up to find their nephew on the front step, but Privet Drive had hardly changed at all. The sun rose on the same tidy front gardens and lit up the brass number four on the Dursleys' front door; it crept into their living room, which was almost exactly the same as it had been on the night when Mr.Dursley had seen that fateful news report about the owls. Only the photographs on the mantelpiece really showed how much time had passed. Ten years ago, there had been lots of pictures of what looked like a large pink beach ball wearing different - colored bonnets - but Dudley Dursley was no longer a baby, and now the photographs showed a large blond boy riding his first bicycle, on a carousel at the fair, playing a computer game with his father, being hugged and kissed by his mother. The room held no sign at all that another boy lived in the house, too."
 // let rjtext = "But, soft! what light through yonder window breaks? It is the east, and Juliet is the sun. Arise, fair sun, and kill the envious moon, Who is already sick and pale with grief, That thou her maid art far more fair than she: Be not her maid, since she is envious; Her vestal livery is but sick and green And none but fools do wear it; cast it off. It is my lady, O, it is my love! O, that she knew she were! She speaks yet she says nothing: what of that? Her eye discourses; I will answer it. I am too bold, 'tis not to me she speaks: Two of the fairest stars in all the heaven, Having some business, do entreat her eyes To twinkle in their spheres till they return. What if her eyes were there, they in her head? The brightness of her cheek would shame those stars, As daylight doth a lamp; her eyes in heaven Would through the airy region stream so bright That birds would sing and think it were not night. See, how she leans her cheek upon her hand! O, that I were a glove upon that hand, That I might touch that cheek!"
 // let sarah = {
