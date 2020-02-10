2cc6ad32b7393b4f469cfb1ccfc2b804bcf65387
Alex Segers <a.segers.dev@gmail.com>
2020-02-10 13:51:52 -0800

    break styles into separate files

---
+++ frontend/src/styled/base/Page.styled.jsx
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+
+export const Page = styled.section`
+
+`;
+
+export const PageTitle = styled.h1`
+  padding: 2rem 0;
+  font-size: 6rem;
+  font-weight: 700;
+`;
