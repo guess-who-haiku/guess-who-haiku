ca92e7b1f74fb71a3d49460c8f7f8774ae71bd22
Tatiana Faramarzi <tfaramarzi@gmail.com>
2020-03-24 17:19:41 -0700

    Implement route-based code splitting to optimize load time

--- frontend/src/components/App.jsx
+++ frontend/src/components/App.jsx
@@ -1,31 +1,35 @@
-import React from 'react';
+import React, { Suspense, lazy } from 'react';
 import { Switch, Route } from 'react-router-dom';
 import {ProtectedRoute } from 'util/route_util';
 
 import ModalContainer from './modal/ModalContainer';
 import Navbar from './nav/Navbar';
-import LandingPage from './landing/LandingPageContainer';
-import Scoreboard from './scoreboard/ScoreboardContainer';
-import SolveHaiku from './solve_haiku/SolveHaikuContainer';
-
-import MyHaikus from './haiku_views/MyHaikus';
-import MyChallenges from './haiku_views/MyChallenges';
-import About from './about/About';
 import Footer from './footer/Footer';
+import LoadingSpinner from './haiku_views/LoadingSpinner';
+
+import LandingPage from './landing/LandingPageContainer';
+const Scoreboard = lazy(() => import('./scoreboard/ScoreboardContainer'));
+const SolveHaiku = lazy(() => import('./solve_haiku/SolveHaikuContainer'));
+const MyHaikus = lazy(() => import('./haiku_views/MyHaikus'));
+const MyChallenges = lazy(() => import('./haiku_views/MyChallenges'));
+const About = lazy(() => import('./about/About'));
+
 
 const App = () => (
   <>
     <ModalContainer />
     <Navbar />
     <main>
-      <Switch>
-        <Route exact path="/" component={LandingPage} />
-        <Route exact path="/about" component={About} />
-        <Route exact path="/scoreboard" component={Scoreboard} />
-        <ProtectedRoute exact path="/challenges" component={MyChallenges} />
-        <ProtectedRoute exact path="/haikus" component={MyHaikus} />
-        <Route exact path="/haikus/:haikuId" component={SolveHaiku} />
-      </Switch>
+      <Suspense fallback={<LoadingSpinner />}>
+        <Switch>
+          <Route exact path="/" component={LandingPage} />
+          <Route exact path="/about" component={About} />
+          <Route exact path="/scoreboard" component={Scoreboard} />
+          <ProtectedRoute exact path="/challenges" component={MyChallenges} />
+          <ProtectedRoute exact path="/haikus" component={MyHaikus} />
+          <Route exact path="/haikus/:haikuId" component={SolveHaiku} />
+        </Switch>
+      </Suspense>
     </main>
     <footer>
       <Footer />
